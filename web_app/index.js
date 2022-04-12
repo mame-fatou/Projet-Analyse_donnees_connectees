60f0dfc29a885c8b7b2f0613dd19b61ba0a38aa2
Salka18 <90775495+Salka18@users.noreply.github.com>
2022-04-12 14:25:04 +0200

    telechargemt de données de la Consommation mensuelle brute de gaz des grandes Métropoles françaises

--- web_app/index.js
+++ web_app/index.js
@@ -1,5 +1,20 @@
-const express = require('express') // importer le module 
+const express = require('express') // importer le module
 const app = express()
+
+const csv =require('csv-parser')
+const fs =require('fs')
+const results =[];
+// Telechargement de données de la Consommation mensuelle brute de gaz des grandes Métropoles françaises
+
+fs.createReadStream('gaz.csv')
+  .pipe(csv({}))
+  .on('data',(data) =>results.push(data))
+  .on('end',() =>{
+  console.log(results);
+  })
+
+
+// Accés a l'API openweathermap
 
 const axios = require('axios').default
 
@@ -8,7 +23,6 @@ const lon = -8.14
 const api_key = "0e4d74900adb00bf2730328875c55b09"
 
 app.get('/',(req,res)=>{
-    
     axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`)
     .then(response => {
         res.json(response.data)
@@ -18,7 +32,9 @@ app.get('/',(req,res)=>{
     })
 })
 
+
+// serveur
+
 app.listen(3000,()=>{
     console.log('serveur depuis en marche ')
-}) 
-
+})
