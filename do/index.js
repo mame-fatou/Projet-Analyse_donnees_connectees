90a997776cbe08bfd72587b20da79942a44cd96a
CynthiaY <cynthiayagoub@hotmail.com>
2022-04-14 23:53:36 +0200

    Ajout des fichiers pour Swagger

--- do/index.js
+++ do/index.js
@@ -36,28 +36,39 @@ const scrap = async () =>{
         await page.goto(URL);
 		//await page.pdf({ path: 'page.pdf' })
         console.log("goto");
-		await page.waitForSelector('div');
+		await page.waitForSelector('div.table--responsive');
 		console.log("wait");
+		//console.log("items", items);
 		
+		//await c'est pour récupérer une info
 		const data = await page.evaluate(()=>{
 			
 			console.log("evaluate");
 			const list = []
 			//const items = document.querySelectorAll("td");
-			const items = document.querySelectorAll("div > td");
+			const items = document.querySelectorAll('div.table--responsive');
 			
-			console.log("items", items);
+			//const items = document.querySelectorAll("div.table--responsive:nth-child(15) > table.table.table--bordered > tbody > tr > td");
+			//const items = document.querySelectorAll("div.table--responsive:nth-child(15) > table:nth-child(1) > tbody:nth-child(3) > tr:nth-child(1) > td:nth-child(1) > strong:nth-child(1)");
+			//const items = document.querySelectorAll("div.table--responsive:nth-child(15) > table:nth-child(1) > tbody:nth-child(3) > tr:nth-child(1) > td:nth-child(1) > strong:nth-child(1)");
+			//console.log("items", items);
 			for (const item of items) {
-				/*list.push({
-					ville: item.querySelector(".ville td").innerHTML
-					//console.log(ville)
-					})*/
+				list.push({
+					ville: item.querySelector("div.table--responsive:nth-child(19) > table:nth-child(1) > tbody:nth-child(3) > tr:nth-child(1) > td:nth-child(1) > strong:nth-child(1)")?.textContent,
+					//prix: item.querySelector("div#block-agrippa-content > article.article > div > h3")?.textContent,
+					//pri: item.querySelector("#line1 > span:nth-child(1909)")?.textContent,
+					//p: item.querySelector("div.table--responsive:nth-child(15) > table:nth-child(1) > tbody:nth-child(3) > tr:nth-child(1) > td:nth-child(1) > strong:nth-child(1)")?.textContent
+					//console.log(ville);
+					})
 					//console.log("item", item);
+					console.log("items", items);
+					console.log(list);
+					//console.log(data);
 			}
 
 		return list
 		})
-		console.log(data)
+		//console.log(data)
 		await browser.close()
 		} 
 		catch (error) {
