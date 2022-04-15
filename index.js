e82699170713c09001875388b5da3fbdd0c859c6
madiiou <madioubah9955@gmail.com>
2022-04-15 08:46:14 +0200

    Update

--- index.js
+++ index.js
@@ -12,22 +12,20 @@ app.get('/gaz', (req,res) => {
     res.status(200).json(gazs)
 })
 
-//  Pour calculer la consommation moyenne de gaz par ville
+// Root Pour calculer la consommation moyenne de gaz par ville
 app.get('/gaz/nom_officiel_epci/:nom_officiel_epci', (req,res) => {
     const code = req.params.nom_officiel_epci
     let sum =0;
-// n compte le nombre de fois que la ville recherchée est présente dans le document
-    let n=0; // On l'initialise à 0
+// n compte le nombre de fois que la ville recherchée est apparue
+    let n=0;
     let consommation = {}
     const a = []
     for (var i = 0; i < gazs.length; i++) {
         if (gazs[i].nom_officiel_epci === code ) {
            a.push(gazs[i].consommation);
-	// on calcul la somme des consommation trouvée pour la ville 
-           sum = sum + gazs[i].consommation; 
+           sum = sum + gazs[i].consommation;
            n =n+1;  }
 }
-	//Calcul de la moyenne 
     const moyenne = sum/n;
     consommation.nom=code
     consommation.consommation_moyenne = moyenne
@@ -40,7 +38,6 @@ app.get('/gaz/nom_officiel_epci/:nom_officiel_epci', (req,res) => {
 const axios = require('axios').default
 
 app.get('/records/fields/:com_arm_name',(req,res)=>{
-// <<<<<<< Updated upstream
 
     const code = req.params.com_arm_name
 
@@ -59,20 +56,6 @@ app.get('/records/fields/:com_arm_name',(req,res)=>{
         }
         res.status(200).json(carburant)
 
-// =======
-    
-    const code = req.params.com_arm_name
-    
-    axios.get(`https://public.opendatasoft.com/api/records/1.0/search/?dataset=prix-des-carburants-j-1&q=&facet=cp&facet=pop&facet=com_arm_name&facet=automate_24_24&facet=fuel&facet=shortage&facet=update&facet=services&facet=brand&facet=epci_code&facet=epci_name&facet=dep_code&facet=dep_name&facet=reg_code&facet=reg_name&rows=800`)
-    .then(response => {
-        var a = []
-        for (var i = 0; i < response.data.records.length; i++) {
-            if (response.data.records[i].fields.com_arm_name === code ){
-                a.push(response.data.records[i].fields.reg_code)
-            } 
-        }
-        res.status(200).json(a)
-//>>>>>>> Stashed changes
     })
     .catch(Error => {
         console.log(Error.message)
@@ -108,6 +91,3 @@ app.get('/PrixEau/',(req,res) => {
 app.listen(PORT,()=>{
     console.log('serveur en marche ')
 })
-
-    //const date = parseInt(req.params.fields.date)
-   // const parking = parkings.find(parking => parking.date === date)
